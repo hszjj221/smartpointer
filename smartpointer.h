f04d36ed118e76a4a2e894f7aec634595e50a755
hszjj <hszjj221@163.com>
2017-07-05 22:18:50 +0800

    增加引用计数

--- smartpointer.h
+++ smartpointer.h
@@ -2,12 +2,36 @@
 #define _SMARTPOINTER_H_
 
 template<typename T>
-class SmartPointer {
+class BasePointer {
 private:
 	T *pointer_;
+	int ref_count_;
+public:
+	BasePointer(T *p) : pointer_(p), ref_count_(1) {}
+	void IncCount() {
+		++ref_count_;
+	}
+	void DecCount() {
+		--ref_count_;
+	}
+	int GetCount() {
+		return ref_count_;
+	}
+	virtual ~BasePointer() {
+		delete pointer_;
+	}
+};
+
+template<typename T>
+class SmartPointer {
+private:
+	BasePointer *pointer_;
 public:
 	SmartPointer():pointer_(nullptr) {}
-	SmartPointer(T *p):pointer_(p) {}
+	SmartPointer(T *p):pointer_(p) {
+		if (pointer_)
+			pointer_->IncCount();
+	}
 	~SmartPointer() {
 		if (pointer_) {
 			delete pointer_;
@@ -16,11 +40,14 @@ public:
 
 	SmartPointer(const SmartPointer& sp) {
 		pointer_ = sp.pointer_;
+		pointer_->IncCount();
 	}
 	SmartPointer& operator = (const SmartPointer& sp) {
 		if (this == &sp)
 			return *pointer_;
-		if (pointer_)
+		sp.pointer_->IncCount();
+		pointer_->DecCount();
+		if (pointer_->GetCount() == 0)
 			delete pointer_;
 		pointer_ = sp.pointer_;
 		return *this;
